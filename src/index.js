350f77366b5247be6f0857846561858beaad0acb
shinji <shinbey221@gmail.com>
2019-05-02 16:49:45 +0900

    module commint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import _ from 'lodash'
+import * as util from './util.js'
+
+console.log(util.Nijou(2));
+console.log(util.NAME);
 
 function component(){
   const element = document.createElement('div');
